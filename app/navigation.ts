f06987dc608dbd54d758ff6bde516cc102ee32c2
lee <lijinhong@shocknowcom>
2024-10-15 18:39:20 +0800

    调整不重定向

--- app/navigation.ts
+++ app/navigation.ts
@@ -2,7 +2,7 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 
 import { locales } from '../i18n';
 
-export const localePrefix = 'as-needed';
+export const localePrefix = 'always';
 
 // eslint-disable-next-line object-curly-newline
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({ locales, localePrefix });
